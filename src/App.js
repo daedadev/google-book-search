f4c59bdc630166ce92e51117a0c919654bd91cf4
Daniel Moore <wron2@hotmail.com>
2021-12-08 20:02:17 -0800

    Updated formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
