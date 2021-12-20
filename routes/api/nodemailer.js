be88e2fa8f3d9c9bbc2040bb2afe684f622e47fd
Daniel Moore <wron2@hotmail.com>
2021-12-20 15:00:02 -0800

    Added nodemailer module and file to allow for emails to be sent

---
+++ routes/api/nodemailer.js
@@ -0,0 +1,13 @@
+var nodemailer = require("nodemailer");
+require("dotenv").config();
+
+var transporter = nodemailer.createTransport({
+  host: "mail.privateemail.com",
+  service: "mail.privateemail.com",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+module.exports = transporter;
