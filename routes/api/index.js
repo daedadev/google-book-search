fa50256b34da156eccc6fed3354f5eb858fcef7c
Daniel Moore <wron2@hotmail.com>
2021-12-22 13:21:10 -0800

    Added API calls to Utils folder utilizing axios and renamed the main book routes from Get to Book, and calls on api/book

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const loginRoutes = require("./loginRoutes");
-const getRoutes = require("./getRoutes");
+const bookRoutes = require("./bookRoutes");
 const authRoutes = require("./authRoutes");
 
 router.use("/login", loginRoutes);
-router.use("/get", getRoutes);
+router.use("/book", bookRoutes);
 router.use("/auth", authRoutes);
 
 module.exports = router;
