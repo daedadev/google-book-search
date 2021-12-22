93bced6161b374eda70d146aa60118a600cd9aa4
Daniel Moore <wron2@hotmail.com>
2021-12-22 01:23:46 -0800

    Updated code so search bar works and returns search results

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,10 +1,12 @@
 export default function Book({ book }) {
+  console.log(book.volumeInfo);
+
   return (
     <li>
       <h1>{book.volumeInfo.title}</h1>
       <h1>Authors: {book.volumeInfo.authors}</h1>
       <p>{book.volumeInfo.description}</p>
-      <h1>Link To Book: {book.volumeInfo.infolink}</h1>
+      <h1>Link To Book: {book.volumeInfo.infoLink}</h1>
     </li>
   );
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,10 +4,12 @@ import Book from "../components/Book";
 const Search = () => {
   const [bookSearch, setBookSearch] = useState([]);
 
+  const [input, setInput] = useState();
+
   async function searchButtonHandler(event) {
     event.preventDefault();
     const searchResult = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=barney`
+      `https://www.googleapis.com/books/v1/volumes?q=${input}`
     );
     const data = await searchResult.json();
     setBookSearch(data.items);
@@ -18,6 +20,7 @@ const Search = () => {
     <div>
       <form onSubmit={searchButtonHandler}>
         <h1>Search</h1>
+        <input value={input} onInput={(e) => setInput(e.target.value)}></input>
         <button type="submit"></button>
       </form>
       <ul>
