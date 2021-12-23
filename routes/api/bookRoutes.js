76e6920f3e0b23e276263f430c1d176be3cae701
Daniel Moore <wron2@hotmail.com>
2021-12-23 01:37:47 -0800

    Changed the book route descriptors

--- routes/api/bookRoutes.js
+++ routes/api/bookRoutes.js
@@ -2,10 +2,15 @@ const router = require("express").Router();
 const { User, Book } = require("../../models");
 const cors = require("cors");
 
-// Routes all defined on /api/get
+// General CRUD routes
+
+// Routes all defined on /api/book/
 router.get("/", cors(), async (req, res) => {});
 
-// Routes all defined on /api/get/stored
-router.get("/stored", async (req, res) => {});
+// Routes all defined on /api/book/
+router.post("/", cors(), async (req, res) => {});
+
+// Routes all defined on /api/book/delete
+router.get("/delete", cors(), async (req, res) => {});
 
 module.exports = router;
