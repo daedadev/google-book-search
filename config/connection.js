e2294342316fe9e00be684881a2de7878c4870e5
Daniel Moore <wron2@hotmail.com>
2022-01-19 17:01:37 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,15 @@ require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+sequelize = new Sequelize(
+  zcf8igfpwxk1c5u3,
+  wsom8x86u6h38ldb,
+  z6t7ch78r1lh3y0t,
+  {
+    host: "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
