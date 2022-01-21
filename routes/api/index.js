9b114d13f01b1b9810a0ca4a2eb6b85116e213c3
Daniel Moore <wron2@hotmail.com>
2022-01-20 20:45:51 -0800

    Another heroku update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
-const loginRoutes = require("./loginRoutes");
 const bookRoutes = require("./bookRoutes");
-const authRoutes = require("./authRoutes");
 
-router.use("/login", loginRoutes);
 router.use("/book", bookRoutes);
-router.use("/auth", authRoutes);
 
 module.exports = router;
