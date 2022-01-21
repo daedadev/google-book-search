e8b4f00f77b0744571c64c97f492139cf88f6ada
Daniel Moore <wron2@hotmail.com>
2022-01-20 20:38:46 -0800

    Another heroku update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 sequelize.sync({ force: false }).then(() => {
